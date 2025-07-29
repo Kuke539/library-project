e5b6a17e7092f4da0e3e66030f9565c4bc5490a9
Kuke539 <rainbowfedya@gmail.com>
2025-07-29 15:22:13 +0300

    К

--- src/main/java/com/example/libraryproject/controller/Controller.java
+++ src/main/java/com/example/libraryproject/controller/Controller.java
@@ -1,4 +1,34 @@
 package com.example.libraryproject.controller;
 
-public class Controller {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Book;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService service;
+
+    @Autowired
+    public BookController(BookService service) {
+        this.service = service;
+    }
+
+    @   PostMapping
+    public String createBook(@RequestBody Book book) {
+        service.addBook(book.getTitle(), book.getAuthor());
+        return "Книга успешно сохранена.";
+    }
+
+    @GetMapping("/{id}")
+    public Object showBook(@PathVariable int id) {
+        Book book = service.getBookById(id);
+        if (book != null) {
+            return book;
+        } else {
+            return "Книга не найдена.";
+        }
+    }
 }
