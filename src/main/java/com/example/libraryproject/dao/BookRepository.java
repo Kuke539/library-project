342cc095325a0d88668d7bd8d60d315e1f1dd31b
Kuke539 <rainbowfedya@gmail.com>
2025-07-30 15:54:58 +0300

    Убрал схлопывание импортов и отформатировал код.

--- src/main/java/com/example/libraryproject/dao/BookRepository.java
+++ src/main/java/com/example/libraryproject/dao/BookRepository.java
@@ -2,14 +2,14 @@ package com.example.libraryproject.dao;
 
 import com.example.libraryproject.model.dto.BookDTO;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class BookRepository {
 
     private final Map<Integer, BookDTO> database = new HashMap<>();
     private int currentId = 1;
-
 
 
     public void save(BookDTO book) {
