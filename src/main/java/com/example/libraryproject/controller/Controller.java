e12155235be55ebf805500fa21cb61861af53614
Kuke539 <rainbowfedya@gmail.com>
2025-07-30 15:42:55 +0300

    Постарался исправить твои замечания.

--- src/main/java/com/example/libraryproject/controller/Controller.java
+++ src/main/java/com/example/libraryproject/controller/Controller.java
@@ -1,34 +1,41 @@
 package com.example.libraryproject.controller;
 
+import com.example.libraryproject.service.BookService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.print.Book;
 
-@RestController
 @RequestMapping("/books")
-public class BookController {
+public class Controller {
+    public class BookController {
 
-    private final BookService service;
+        private final BookService service;
 
-    @Autowired
-    public BookController(BookService service) {
-        this.service = service;
-    }
-
-    @   PostMapping
-    public String createBook(@RequestBody Book book) {
-        service.addBook(book.getTitle(), book.getAuthor());
-        return "Книга успешно сохранена.";
-    }
-
-    @GetMapping("/{id}")
-    public Object showBook(@PathVariable int id) {
-        Book book = service.getBookById(id);
-        if (book != null) {
-            return book;
-        } else {
-            return "Книга не найдена.";
+        @RestController
+        @AllArgsConstructor
+        public class BookController {
+            private final BookService service;
         }
-    }
-}
+
+        @PostMapping
+        public String createBook(@RequestBody Book Book) {
+            service.addBook(Book);
+            return "Book created successfully";
+        }
+
+        @GetMapping("/{id}")
+        public Object showBook(@PathVariable int id) {
+            Book book = service.getBookById(id);
+            if (book != null) {
+                return book;
+                {
+                    return "Книга не найдена.";
+                }
+            }
