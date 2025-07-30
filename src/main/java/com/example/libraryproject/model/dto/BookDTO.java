81caf2d6ff779269aaa8417929511cac306d09db
Kuke539 <rainbowfedya@gmail.com>
2025-07-30 15:50:29 +0300

    Отформатировал данный код согласно аннотация, что ты указал.

--- src/main/java/com/example/libraryproject/model/dto/BookDTO.java
+++ src/main/java/com/example/libraryproject/model/dto/BookDTO.java
@@ -1,36 +1,16 @@
 package com.example.libraryproject.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BookDTO {
     private int id;
     private String title;
     private String author;
-
-    public BookDTO() {}
-
-    public BookDTO(int id, String title, String author) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-    }
-
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
